e381b80ce5b77aae2912a7d0336f1a5b2db22add
Tang Jianhua <shmp2005@163.com>
2018-07-07 19:22:52 +0800

    fix bug

---
+++ src/main/java/com/zhongba/sbrabbitmq/SbRabbitmqApplication.java
@@ -0,0 +1,17 @@
+package com.zhongba.sbrabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SbRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRabbitmqApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/zhongba/sbrabbitmq/SbRabbitmqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhongba.sbrabbitmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbRabbitmqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
